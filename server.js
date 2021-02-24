fee8bc7bfae36dda0e628eeb44ec799652e75d66
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-02-24 12:34:38 +0200

    3:30-before

--- server.js
+++ server.js
@@ -14,95 +14,55 @@
 //why that :3 packages are looking for funding
 // run `npm fund` for details
 require('dotenv').config();
-
 const express = require('express');
 const server = express();
 const cors = require('cors');
-
 const superagent = require('superagent');
-const pg=require('pg');
+const pg = require('pg');
+const { query } = require('express');
 const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL
-  });
-//   ,ssl: { rejectUnauthorized: false },
+    connectionString: process.env.database_url
+});
+//   ,ssl: { rejectUnauthorized: false }, //herocu
 
 server.use(cors());
 const PORT = process.env.PORT || 3030;
-
 /////////////////////////////////////////////////////////////
-
-server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
 server.get('/parks', parkHandler);
-
 ////////////////////////////////////////////////////////////
-
+let key = process.env.locationKey;
+// server.get(url, locationHandler);
+server.get('/location', locationHandler);
 
 ///////////////////////////////////////////////////////////
 function locationHandler(req, res) {
-   
-        const city = req.query.city;
-
- let key = process.env.locationKey;
     let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-///
-let SQL=`SELECT * FROM locations where search_query = "${city}";`;
-client.query(SQL).then(results=>{
-//if (results.rows.length===0){bring from api >>>pass the search to scheme}
-//else()
-if (results.rows.length === 0){
-        superagent.get(url).then(locData => {
-     
-        const locationData = new Location(city,locData.body[0]);
-       res.send(locationData);
-})
-.catch(() => {
-    errorHandler(`Error`, req, res);
-  });
-}else{
-    server.get('/addLocation',(req,res)=>{
-        let formatted_query=req.query.formatted_query;
-        let name=req.query.search_query;
-        let latitude=req.query.latitude;
-        let longitude=req.query.longitude;
-        let SQL=`INSERT INTO locations VALUES($1,$2,$3,$4) RETURNING *;`;
-        let safeValues=[formatted_query,name,latitude,longitude];
-        client.query(SQL,safeValues).then((results)=>{
-            res.send(results.rows);
-        
-        })
-        .catch((error)=>{
-            res.send('this is a big biiig ',error.message)
-        })
-        
+    const values = [req.query.database_url];
+    // const city = req.query.city;
+    client.query(query, values).then(results => {
+//pass it or add it ......database 
+        if (results.rows.length === 0) {
+            superagent.get(url).then(locData => {
+                const locationData = new Location(req.query.database_url, locData.body.results[0].formatted_query, locData.body.results[0].latitude, locData.body.results[0].longitude);
+                const query = `SELECT * FROM locations where search_query = "${city}";`;
+                let values = Object.values(values);
+
+                res.send(locationData);
+            })
+                .catch(() => {
+                    errorHandler(`Error`, req, res);
+                });
+        }
+        res.send(results.rows);
+    })
+        .catch((error) => {
+            res.send('this is', error.message)
         })
 }
-    
-    res.send(results.rows);
-})
-.catch((error)=>{
-    res.send('this is',error.message)
-})
-/////
-
-}
-
-server.get('location',(req,res)=>{
-    let SQL=`SELECT *FROM location;`;
-    client.query(SQL).then(results=>{
-        res.send(results.rows)
-    })
-    .catch((error)=>{
-        res.send('ther is a line error ',error.message)
-    })
-})
-
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function weatherHandler(req, res) {
     const city = req.query.city;
-
     getWeather(city).then(weatherData => {
         res.status(200).json(weatherData);
     })
@@ -115,41 +75,33 @@ function parkHandler(req, res) {
         res.status(200).json(parkData);
     })
 }
-//////////////////////////////////////////////////////////////
-
 function getWeather(city) {
     let key = process.env.WeatherKey;
     let url = `https://api.weatherbit.io/v2.0/history/daily?&city=${city_name},NC&start_date=2021-02-18&end_date=2021-02-19&key=${key}`;
     return superagent.get(url).then(weatherDayData => {
-
         const weatherData = new Weather(weatherDayData);
         return weatherData;
     });
 }
-
 function getPark(city) {
     let key = process.env.parkKey;
     let url = `us1.locationiq.com/v1/search.php?key=${name}&q=${key}&format=json`;
     return superagent.get(url).then(parkDayData => {
-
         const parkData = new Park(parkDayData);
         return parkData;
     });
 }
-/////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function Location(city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData.display_name;
     this.latitude = geoData.lat;
     this.longitude = geoData.lon;
-
 }
 function Weather(city_name, temp, datetime) {
     this.city_name = city_name;
     this.temp = day.data.temp;
     this.datetime = new Date(data.datetime).toString().slice(0, 15);
-
 }
 function Park(name) {
     this.name = full_name;
@@ -157,20 +109,22 @@ function Park(name) {
     this.parkUrl = data.url;
     this.directionsUrl = data.directionsUrl;
     this.fee = data.fees;
-
 }
-
-    // const client=new pg.client(process.env.database_url);//1st step local
-    client.connect().then(()=>{
-server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-      });
-    })
-    .catch((error)=>{
-
-    res.send('error in line 175 ',error.message)
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+client.connect().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`)
+    });
 })
+    .catch((error) => {
 
-/////////////////////////////////////////////////////////////////////////////
+        res.send('nicely have an error successfully ', error.message)
 
+    })
+
+/////error:jquery.min.js:4 GET http://localhost:3000/location?city=amman 500 (Internal Server Error)
+// send @ jquery.min.js:4
+// ajax @ jquery.min.js:4
+// fetchCityData @ app.js:73
+// dispatch @ jquery.min.js:3
+// q.handle @ jquery.min.js:3
