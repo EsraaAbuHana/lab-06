e0398b81383a89e5822a14300fc7cdbba789b553
EsraaAbuHana <esraa.abuhana@gmail.com>
2021-02-27 23:20:36 +0200

    missing movie yelp

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 'use strict';
 
-//weather
-//https://api.weatherbit.io/v2.0/history/daily?&city=Raleigh,NC&start_date=2021-02-18&end_date=2021-02-19&key=API_KEY
-
-//or
-//https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&country=US&start_date=2021-02-18&end_date=2021-02-19&key=API_KEY
-//(weather by city name) &city=Raleigh,NC&start_date=2021-02-18&end_date=2021-02-19
-//Park
-//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=SZcUUGdtmXRqUb5BNLV6gQa6TIvkVF0mLVsdFmWd
-//location
-//https://us1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
-
-//why that :3 packages are looking for funding
-// run `npm fund` for details
 require('dotenv').config();
 const express = require('express');
 const server = express();
@@ -23,121 +10,119 @@ const { query } = require('express');
 const client = new pg.Client({
     connectionString: process.env.database_url
 });
-//   ,ssl: { rejectUnauthorized: false }, //herocu
 
 server.use(cors());
 const PORT = process.env.PORT || 3030;
+
 /////////////////////////////////////////////////////////////
+server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
 server.get('/parks', parkHandler);
+server.get('/yelp', yelpHandler);
+server.get('/movie', movieHandler);
+function yelpHandler(params) {
+    
+}
+function movieHandler(params) {
+    
+}
+
 ////////////////////////////////////////////////////////////
-// server.get(url, locationHandler);
-server.get('/location', locationHandler);
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// function locationHandler(req, res) {
-//     let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-//     const values = [req.query.database_url];
-//     // const city = req.query.city;
-//     client.query(query, values).then(results => {
-// //pass it or add it ......database 
-//         if (results.rows.length === 0) {
-//             superagent.get(url).then(locData => {
-//                 const locationData = new Location(req.query.database_url, locData.body.results[0].formatted_query, locData.body.results[0].latitude, locData.body.results[0].longitude);
-//                 const query = `SELECT * FROM locations where search_query = "${city}";`;
-//                 let values = Object.values(values);
-
-//                 res.send(locationData);
-//             })
-//                 .catch(() => {
-//                     errorHandler(`Error`, req, res);
-//                 });
-//         }
-//         res.send(results.rows);
-//     })
-//         .catch((error) => {
-//             res.send('this is error', error.message)
-//         })
-// }
-/////// 
-// const city = req.query.city;
-
-server.get('/location', locationHandler);
-//pass it or add it ......database 
 function locationHandler(req, res) {
-    //  const values = [req.query.database_url];
+
     let city = req.query.city;
+    let SQL = `SELECT * FROM locations WHERE search_query='${city}';`;
 
+    let key = process.env.locationKey;
+    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
 
-    client.query(query, values).then(results => {
+    client
+        .query(SQL)
+        .then(results => {
 
-        if (location.rows.length === 0 || city !== req.query.city) {
-            let key = process.env.locationKey;
-            let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-            superagent.get(url, (req, res) => {
-                let SQL = `INSERT INTO location (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4);`;
-                let locationData = new Location(req.query.database_url, locData.body.results[0].formatted_query, locData.body.results[0].latitude, locData.body.results[0].longitude);
+            if (results.rows.length === 0) {
+                superagent.get(url).then((locData) => {
 
-                client.query(SQL, locationData).then((results) => {
-                    res.send(results.rows);
-                })
-                    .catch((error) => {
-                        res.send('this is add to schema error ', error.message)
+                    const locObj = new Location(city, locData.body[0]);
+
+                    const insertQuery = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4) RETURNING *;`;
+
+                    let saveValues = [city, locObj.formatted_query, locObj.latitude, locObj.longitude];
+                    client.query(insertQuery, saveValues).then((getLocation) => {
+                        res.json(getLocation.rows[0]);
+                    }).catch((error) => {
+                        res.send('nicely have an error successfully ', error.message);
                     })
-            })
-        } else {
-            if (city === req.query.city) {
-                const SQL = `SELECT * FROM locations where search_query = "${city}";`;
-                client.query(SQL).then(results => {
-                    res.send(results.rows);
+                }).catch((error) => {
+                    res.send('nicely have an error successfully ', error.message);
                 })
-                    .catch((error) => {
-                        res.send('get from schema error', error.message)
-                    })
-                //   }else{ }  
             }
-        }
-        res.send(results.rows);
-    })
+            else if (result.rows[0].search_query === city) {
+                const getObject = new Location(result.rows[0].search_query, result.rows[0]);
+                res.json(getObject);
+            }
+
+
+        }).catch(() => {
+            res.send('nicely have an error successfully ', error.message);
+        })
 }
-///////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 function weatherHandler(req, res) {
-    const city = req.query.city;
-    getWeather(city).then(weatherData => {
-        res.status(200).json(weatherData);
-    })
-}
-
-function parkHandler(req, res) {
-    const city = req.query.city;
-
-    getPark(name).then(parkData => {
-        res.status(200).json(parkData);
-    })
-}
-function getWeather(city) {
+    let ArrOfWeatherDays=[];
+    const city = req.query.search_query;
     let key = process.env.WeatherKey;
-    let url = `https://api.weatherbit.io/v2.0/history/daily?&city=${city_name},NC&start_date=2021-02-18&end_date=2021-02-19&key=${key}`;
-    return superagent.get(url).then(weatherDayData => {
-        const weatherData = new Weather(weatherDayData);
-        return weatherData;
-    });
+    let url = `https://api.weatherbit.io/v2.0/history/daily?&city=${city_name}&key=${key}`;
+
+superagent.get(url).then(weatherData=>{
+    ArrOfWeatherDays=weatherData.body.data.map(element=>{
+        return new Weather(element);
+    })
+}).catch((error)=>{
+    res.send('nicely have an error successfully ', error.message);
+})
+
+
 }
-function getPark(city) {
+let ArrOfParks=[];
+function parkHandler(req, res) {
+    ParkCode=req.query.latitude+','+query.longitude;
+    // const city = req.query.city;
     let key = process.env.parkKey;
-    let url = `us1.locationiq.com/v1/search.php?key=${name}&q=${key}&format=json`;
-    return superagent.get(url).then(parkDayData => {
-        const parkData = new Park(parkDayData);
-        return parkData;
+    let url = `https://developer.nps.gov/api/v1/parks?parkCode=${ParkCode}&limit=3&api_key=${key}`;
+     superagent.get(url).then(parkDayData => {
+
+        ArrOfParks=parkDayData.body.data.map(element=>{
+        parkObj = new Park(parkDayData);
+        return parkObj;
     });
+    res.send(ArrOfParks);
+    }).catch((error)=>{
+        res.send('nicely have an error successfully ', error.message);
+    })
+    // getPark(name).then(parkData => {
+    //     res.status(200).json(parkData);
+    // })
 }
+// function getWeather(city) {
+//     // let key = process.env.WeatherKey;
+//     // let url = `https://api.weatherbit.io/v2.0/history/daily?&city=${city_name},NC&start_date=2021-02-18&end_date=2021-02-19&key=${key}`;
+//     return superagent.get(url).then(weatherDayData => {
+//         const weatherData = new Weather(weatherDayData);
+//         return weatherData;
+//     });
+// }
+// function getPark(city) {
+   
+// }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function Location(city, geoData) {
-    this.search_query = city;
-    this.formatted_query = geoData.display_name;
-    this.latitude = geoData.lat;
-    this.longitude = geoData.lon;
+    this.search_query = city  ;
+    this.formatted_query = geoData.display_name ||geoData.formatted_query ;
+    this.latitude = geoData.lat ||geoData.latitude ;
+    this.longitude = geoData.lon || geoData.longitude;
 }
 function Weather(city_name, temp, datetime) {
     this.city_name = city_name;
@@ -151,6 +136,22 @@ function Park(name) {
     this.directionsUrl = data.directionsUrl;
     this.fee = data.fees;
 }
+function Yelp(yelpData) {
+    // https://`<partner_domain_path>/<partner_business_id>/?opportunity_token=<opportunity_token>`&yelp_site=m&yelp_locale=en_US
+ 
+    
+      
+}
+function Movie(MoData)
+{
+    
+    // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&query=whiplash&language=de-DE&region=DE
+
+  
+  
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 client.connect().then(() => {
     server.listen(PORT, () => {
@@ -169,3 +170,19 @@ client.connect().then(() => {
 // fetchCityData @ app.js:73
 // dispatch @ jquery.min.js:3
 // q.handle @ jquery.min.js:3
+
+//weather
+//https://api.weatherbit.io/v2.0/history/daily?&city=Raleigh,NC&start_date=2021-02-18&end_date=2021-02-19&key=API_KEY
+
+//or
+//https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&country=US&start_date=2021-02-18&end_date=2021-02-19&key=API_KEY
+//(weather by city name) &city=Raleigh,NC&start_date=2021-02-18&end_date=2021-02-19
+//Park
+//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=SZcUUGdtmXRqUb5BNLV6gQa6TIvkVF0mLVsdFmWd
+//location
+//https://us1.locationiq.com/v1/search.php?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+
+//why that :3 packages are looking for funding
+// run `npm fund` for details
+// function locationHandler(req, res) {
+//     let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
